30ebf420af1461b1928ba5bf3d4f7cf1d8e0cec3
Jordan Schalm <jordan.schalm@gmail.com>
2016-10-16 20:39:26 -0700

    add handler for simple file upload

--- main.go
+++ main.go
@@ -3,13 +3,18 @@
 package main
 
 import (
-	"fmt" // standard formatting library, `printf` and so on
+	"fmt"
+	"io"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
 )
+
+// Max file size to store in memory. 100MB
+const MAX_FILE_SIZE = 6 << 24
 
 // Functions with lowercase names are private to the package.
 // Uppercase names are public
@@ -37,12 +42,38 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Sift. Your random number is %d", n)
 }
 
+// Handles uploads of multipart forms. Files should have form name `feedback`.
+// Uploaded files are stored in `./uploads`
+func FeedbackFormHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(MAX_FILE_SIZE)
+	file, handler, err := r.FormFile("feedback")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer file.Close()
+	f, err := os.Create(handler.Filename)
+	if err != nil {
+		fmt.Println("error creating file", err.Error())
+		return
+	}
+
+	defer f.Close()
+	_, err = io.Copy(f, file)
+	if err != nil {
+		fmt.Println("error copying file", err.Error())
+	}
+}
+
 // `main` function is the entry point, just like in C
 func main() {
 	// Create a new router, routers handle sets of logically related routes
 	router := mux.NewRouter()
 	// Add a handler for the root route
 	router.HandleFunc("/", IndexHandler)
+	// Handler for the feedback upload route
+	router.HandleFunc("/feedback", FeedbackFormHandler).Methods("POST")
 	// Create an http server on port 9090 and start serving using our router.
 	http.ListenAndServe(":9090", router)
 }
