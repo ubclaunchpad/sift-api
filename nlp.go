60daabe04aca1428ea4d1f3ad3af8439ee8b0398
Jordan Schalm <jordan.schalm@gmail.com>
2016-10-19 21:29:12 -0700

    add method for running nlp jobs

---
+++ nlp.go
@@ -0,0 +1,29 @@
+// Manages running NLP jobs.
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func RunJob(name string, payload interface{}) (map[string]interface{}, error) {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post("http://localhost:5000/"+name, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
